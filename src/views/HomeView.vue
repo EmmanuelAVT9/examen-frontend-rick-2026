b7c8ac66fe043e0952fe6438499693bf84bb29cb
EmmanuelAVT9 <alexsoge@gmail.com>
2026-05-17 15:09:10 -0600

    feat: Added FormKit components to App and HomeView templates

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,4 +2,15 @@
 
 <template>
   <main>Home View</main>
+  <p>Input prueba sencillo</p>
+  <FormKit type="text" />
+  <p>Input prueba con props</p>
+  <FormKit
+    type="text"
+    name="name"
+    id="name"
+    label="Name"
+    help="Your full name"
+    placeholder="“Jon Doe”"
+  />
 </template>
