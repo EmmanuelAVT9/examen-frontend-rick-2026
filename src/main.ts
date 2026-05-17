3ed6ad7ddf40b306594cf28626ad9c8884f62c60
EmmanuelAVT9 <alexsoge@gmail.com>
2026-05-17 15:01:32 -0600

    feat: Install and configure FormKit w/ theme genesis and move file index.css

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
-import '@/assets/styles/index.css'
+import './assets/styles/index.css';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import { plugin, defaultConfig } from '@formkit/vue';
+import '@formkit/themes/genesis';
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +12,8 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(plugin, defaultConfig({
+    theme: 'genesis'
+}))
 
 app.mount('#app')
