8b16f6976e028e464841a0930e9c1c5395417555
EmmanuelAVT9 <alexsoge@gmail.com>
2026-05-17 22:13:56 -0600

    chore: resolve merge conflicts and sync with dev branch assets

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,16 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <main>Home View</main>
+  <p>Input prueba sencillo</p>
+  <FormKit type="text" />
+  <p>Input prueba con props</p>
+  <FormKit
+    type="text"
+    name="name"
+    id="name"
+    label="Name"
+    help="Your full name"
+    placeholder="“Jon Doe”"
+  />
 </template>
