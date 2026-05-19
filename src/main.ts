05b43fd4ffb9b70ecd6726cf58954676dc64b9b5
EmmanuelAVT9 <alexsoge@gmail.com>
2026-05-18 19:35:37 -0600

    feat: added pinia plugin persisted state and created pinia instance

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import './assets/styles/index.css';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 import { plugin, defaultConfig } from '@formkit/vue';
 import '@formkit/themes/genesis';
 
@@ -9,6 +10,9 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+const pinia = createPinia();
+
+pinia.use(piniaPluginPersistedstate);
 
 app.use(createPinia())
 app.use(router)
